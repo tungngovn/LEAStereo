c797c0be42229c3cad039b1cbc9657930253d6b3
tungngovn <tung.ngothanh.hust@gmail.com>
2021-08-07 15:05:41 +0700

    Tuple in input stack

--- export.py
+++ export.py
@@ -348,7 +348,7 @@ def test_cpp(leftname, rightname, savename):
     temp = np.flipud(temp)
 
     # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
-    input_model = torch.stack(input1, input2)
+    input_model = torch.stack((input1, input2))
     traced_script_module = torch.jit.trace(model, input_model)
 
     # traced_script_module.save("traced_resnet_model.pt")
