ccc2d9d0cbc33c55aa36accaeeeef1f3816ee409
tungngovn <tung.ngothanh.hust@gmail.com>
2021-08-24 00:33:43 +0700

    Edit hyperparameters

--- train_apolloscape.sh
+++ train_apolloscape.sh
@@ -1,12 +1,13 @@
 CUDA_VISIBLE_DEVICES=0,1,2 python train.py \
                 --batch_size=3 \
                 --testBatchSize=1 \
-                --crop_height=384 \
-                --crop_width=1248 \
+                --crop_height=288 \
+                --crop_width=576 \
                 --maxdisp=192 \
                 --threads=8 \
                 --dataset='apolloscape' \
-                --save_path='./run/Apolloscape/' \
+                --milestones=[10,20,30,45,60,75,90] \
+                --save_path='./run/Apolloscape/2nd/' \
                 --resume='./run/sceneflow/best/checkpoint/best.pth' \
                 --fea_num_layer 6 --mat_num_layers 12 \
                 --fea_filter_multiplier 8 --fea_block_multiplier 4 --fea_step 3  \
@@ -15,6 +16,6 @@ CUDA_VISIBLE_DEVICES=0,1,2 python train.py \
                 --cell_arch_fea='run/sceneflow/best/architecture/feature_genotype.npy' \
                 --net_arch_mat='run/sceneflow/best/architecture/matching_network_path.npy' \
                 --cell_arch_mat='run/sceneflow/best/architecture/matching_genotype.npy' \
-                --nEpochs=800 2>&1 |tee ./run/Apolloscape/log_2nd.txt
+                --nEpochs=100 2>&1 |tee ./run/Apolloscape/log_3rd.txt
 
                #--resume='./run/Kitti12/best/best_1.16.pth'
