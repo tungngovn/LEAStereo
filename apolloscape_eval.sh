e9f2ea5fb33f4138cfdb9c3c51c248efb84ef6a8
tungngovn <tung.ngothanh.hust@gmail.com>
2021-08-30 15:01:12 +0700

    Code to evaluate

---
+++ apolloscape_eval.sh
@@ -0,0 +1,21 @@
+CUDA_VISIBLE_DEVICES=0,1,2 python eval.py \
+                --batch_size=4 \
+                --testBatchSize=1 \
+                --crop_height=288 \
+                --crop_width=576 \
+                --maxdisp=192 \
+                --threads=8 \
+                --dataset='apolloscape' \
+                --milestones=[10,20,30,45,60,75,90] \
+                --save_path='./run/Apolloscape/2nd/' \
+                --resume='./run/Apolloscape/2nd/best.pth' \
+                --fea_num_layer 6 --mat_num_layers 12 \
+                --fea_filter_multiplier 8 --fea_block_multiplier 4 --fea_step 3  \
+                --mat_filter_multiplier 8 --mat_block_multiplier 4 --mat_step 3  \
+                --net_arch_fea='run/sceneflow/best/architecture/feature_network_path.npy' \
+                --cell_arch_fea='run/sceneflow/best/architecture/feature_genotype.npy' \
+                --net_arch_mat='run/sceneflow/best/architecture/matching_network_path.npy' \
+                --cell_arch_mat='run/sceneflow/best/architecture/matching_genotype.npy' \
+                --nEpochs=100 # 2>&1 |tee ./run/Apolloscape/log_3rd.txt
+
+               #--resume='./run/Kitti12/best/best_1.16.pth'
